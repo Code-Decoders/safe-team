92300fb6c5e107999a169971ab825a49158f8f52
Maadhav Sharma <maadhav2001@gmail.com>
2023-03-27 08:35:58 +0530

    Create safe wallet for all team members #4

---
+++ hooks/useSafeWallet.js
@@ -0,0 +1,47 @@
+import Web3 from "web3";
+import Web3Adapter from "@safe-global/safe-web3-lib";
+import SafeServiceClient from "@safe-global/safe-service-client";
+import { SafeFactory } from "@safe-global/safe-core-sdk";
+
+/*
+    This is a custom hook that can be used to create a Safe wallet.
+
+    usage:
+
+    const { create } = useSafeWallet();
+    create(await safeAuth.getProvider() , [safeAuth.getAddress(), ...teamMembers]);
+*/
+
+const useSafeWallet = () => {
+  const create = async (provider, owners) => {
+    const web3 = new Web3(provider);
+
+    const address = (await web3.eth.getAccounts())[0];
+
+    const ethAdapter = new Web3Adapter({
+      web3,
+      signerAddress: address,
+    });
+
+    const safeFactory = await SafeFactory.create({
+      ethAdapter: ethAdapter,
+    });
+
+    const safeAccountConfig = {
+      owners: owners,
+      threshold: owners.length,
+    };
+
+    const safeSdkOwner = await safeFactory.deploySafe({ safeAccountConfig });
+
+    const safeAddress = safeSdkOwner.getAddress();
+
+    console.log("Your Safe has been deployed:");
+    console.log(`https://goerli.etherscan.io/address/${safeAddress}`);
+    console.log(`https://app.safe.global/gor:${safeAddress}`);
+  };
+
+  return { create };
+};
+
+export default useSafeWallet;
