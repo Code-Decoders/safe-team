17ce412083a6aa4935a5b20cdb03a36281229d0e
Maadhav Sharma <maadhav2001@gmail.com>
2023-03-30 12:30:49 +0530

    implement approve and reject transaction buttons #17

--- pages/dashboard/wallet.js
+++ pages/dashboard/wallet.js
@@ -5,7 +5,7 @@ import { Button, Icon } from "../../components/GnosisReact";
 import DashboardLayout from "../../components/layout/DashboardLayout";
 import useRampKit from "../../hooks/useRampKit";
 import useTransaction from "../../hooks/useTransaction";
-import generateERC20Tx from "../../lib/erc20";
+import { generateERC20Tx, getBalance } from "../../lib/erc20";
 import styles from "../../styles/Wallet.module.css";
 
 const Wallet = () => {
@@ -17,113 +17,18 @@ const Wallet = () => {
     getPendingTransactions,
     approveTransaction,
     proposeTransaction,
+    rejectTransaction,
     loading,
     getEthSigner,
   } = useTransaction();
 
   const [members, setMembers] = React.useState([]);
 
-  const [pendingTransactions, setPendingTransactions] = React.useState([
-    {
-      safe: "0xCB8eC99b9647c23C0F52D30f320bBf60a33D08B6",
-      to: "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",
-      value: "0",
-      data: "0x8d80ff0a0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000013200466595626333c55fa7d7ad6265d46ba5fdbbdd9900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb000000000000000000000000f96b7ffd86d10106e986ddafaefb02c6ef4424dd0000000000000000000000000000000000000000000000000de0b6b3a764000000466595626333c55fa7d7ad6265d46ba5fdbbdd9900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb0000000000000000000000004fd3d5db6691c94dbe26302a1b49de25410bccb50000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000",
-      operation: 1,
-      gasToken: "0x0000000000000000000000000000000000000000",
-      safeTxGas: 0,
-      baseGas: 0,
-      gasPrice: "0",
-      refundReceiver: "0x0000000000000000000000000000000000000000",
-      nonce: 10,
-      executionDate: null,
-      submissionDate: "2023-03-30T03:20:10.754449Z",
-      modified: "2023-03-30T03:20:11.549010Z",
-      blockNumber: null,
-      transactionHash: null,
-      safeTxHash:
-        "0x034024d60969f20f5f6e8ddf46f960206187a60ff1310514c51863ac28ad62de",
-      executor: null,
-      isExecuted: false,
-      isSuccessful: null,
-      ethGasPrice: null,
-      maxFeePerGas: null,
-      maxPriorityFeePerGas: null,
-      gasUsed: null,
-      fee: null,
-      origin: "SafeTeam",
-      dataDecoded: {
-        method: "multiSend",
-        parameters: [
-          {
-            name: "transactions",
-            type: "bytes",
-            value:
-              "0x00466595626333c55fa7d7ad6265d46ba5fdbbdd9900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb000000000000000000000000f96b7ffd86d10106e986ddafaefb02c6ef4424dd0000000000000000000000000000000000000000000000000de0b6b3a764000000466595626333c55fa7d7ad6265d46ba5fdbbdd9900000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb0000000000000000000000004fd3d5db6691c94dbe26302a1b49de25410bccb50000000000000000000000000000000000000000000000000de0b6b3a7640000",
-            valueDecoded: [
-              {
-                operation: 0,
-                to: "0x466595626333c55fa7d7Ad6265D46bA5fDbBDd99",
-                value: "0",
-                data: "0xa9059cbb000000000000000000000000f96b7ffd86d10106e986ddafaefb02c6ef4424dd0000000000000000000000000000000000000000000000000de0b6b3a7640000",
-                dataDecoded: {
-                  method: "transfer",
-                  parameters: [
-                    {
-                      name: "to",
-                      type: "address",
-                      value: "0xF96b7fFd86d10106e986DdAfaefb02c6ef4424dd",
-                    },
-                    {
-                      name: "value",
-                      type: "uint256",
-                      value: "1000000000000000000",
-                    },
-                  ],
-                },
-              },
-              {
-                operation: 0,
-                to: "0x466595626333c55fa7d7Ad6265D46bA5fDbBDd99",
-                value: "0",
-                data: "0xa9059cbb0000000000000000000000004fd3d5db6691c94dbe26302a1b49de25410bccb50000000000000000000000000000000000000000000000000de0b6b3a7640000",
-                dataDecoded: {
-                  method: "transfer",
-                  parameters: [
-                    {
-                      name: "to",
-                      type: "address",
-                      value: "0x4FD3d5db6691c94DBe26302A1b49dE25410bCCb5",
-                    },
-                    {
-                      name: "value",
-                      type: "uint256",
-                      value: "1000000000000000000",
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        ],
-      },
-      confirmationsRequired: 2,
-      confirmations: [
-        {
-          owner: "0x4FD3d5db6691c94DBe26302A1b49dE25410bCCb5",
-          submissionDate: "2023-03-30T03:20:11.549010Z",
-          transactionHash: null,
-          signature:
-            "0x485aa10d099f4e3708aedaaeebc55289bfbb1ae19bf8e6f70975e310dae8c0870ef7a820a0b4b854e12d14b3e846c8cfec7528595cf4d952fa840f7324a1cd5c1f",
-          signatureType: "ETH_SIGN",
-        },
-      ],
-      trusted: true,
-      signatures: null,
-    },
-  ]);
+  const [pendingTransactions, setPendingTransactions] = React.useState([]);
 
   const [user, setUser] = React.useState("");
+
+  const [balance, setBalance] = React.useState("");
 
   const stripeRootRef = React.useRef(null);
 
@@ -168,11 +73,14 @@ const Wallet = () => {
   const getTransactions = async () => {
     if (loading) return;
     const signer = await getEthSigner();
+    getBalance(signer).then((balance) =>
+      setBalance((balance / 10 ** 18).toFixed(2).toString())
+    );
     const address = await signer.getAddress();
     setUser(address);
-    // const pdtxs = await getPendingTransactions();
-    // setPendingTransactions(pdtxs.results);
-    // console.log(pdtxs.results[0]);
+    const pdtxs = await getPendingTransactions();
+    setPendingTransactions(pdtxs.results);
+    console.log(pdtxs.results[0]);
   };
 
   async function getData() {
@@ -182,11 +90,15 @@ const Wallet = () => {
     setUILoading(false);
   }
 
-  function onTransactionApprove(hash) {
-    approveTransaction(pdtxs.results[0].safeTxHash);
+  async function onTransactionApprove(hash) {
+    await approveTransaction(hash);
+    getData();
   }
 
-  function onTransactionReject(hash) { }
+  async function onTransactionReject(hash) {
+    await rejectTransaction(hash);
+    getData();
+  }
 
   useEffect(() => {
     getData();
@@ -197,12 +109,13 @@ const Wallet = () => {
         <div className={styles.walletContainer}>
           <div className={styles.walletHeader}>Total Balance</div>
 
-          <div className={styles.balance}>{
-            // TODO: Fetch balance from backend @Maadhav
-          }$10.00</div>
+          <div className={styles.balance}>
+            {"$ "}
+            {balance}
+          </div>
         </div>
         <div>
-          <div className={styles.transaction}>Pending Transaction</div>
+          <div className={styles.transaction}>Pending Transactions</div>
           <div className={styles.transactionHeader}>
             <div style={{ width: "100px" }}>Nonce</div>
             <div className={styles.tableDivider} />
@@ -224,19 +137,48 @@ const Wallet = () => {
                 {tx.confirmationsRequired}
               </div>
               <div className={styles.tableDivider} />
-              <div style={{ width: '100px', display: 'flex', gap: '0 10px' }}>
-                {
-                  tx.confirmations.find((c) => c.owner === '0x4FD3d5db6691c94DBe26302A1b49dE25410bCCb5') ?
-                    <div><Icon type="awaitingConfirmations" size="md" color="pending" /></div> : <div onClick={onTransactionApprove}>
-                      <Icon type='check' size='md' color='primary' />
-                    </div>
-                }
-                {
-                  tx.confirmations.find((c) => c.owner === '0x4FD3d5db6691c94DBe26302A1b49dE25410bCCb5') ? <></> :
-                    <div onClick={onTransactionReject}>
-                      <Icon type='cross' size='md' color='error' />
-                    </div>
-                }
+              <div style={{ width: "100px", display: "flex", gap: "0 10px" }}>
+                {tx.confirmations.find((c) => c.owner === user) ? (
+                  <div>
+                    <Icon
+                      type="awaitingConfirmations"
+                      size="md"
+                      color="pending"
+                      tooltip="Awaiting Confirmations"
+                    />
+                  </div>
+                ) : (
+                  <div
+                    onClick={() => onTransactionApprove(tx.safeTxHash)}
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Icon
+                      type="circleCheck"
+                      size="md"
+                      color="primary"
+                      tooltip="Approve Transaction"
+                    />
+                  </div>
+                )}
+                {tx.confirmations.find((c) => c.owner === user) ? (
+                  <></>
+                ) : (
+                  <div
+                    onClick={() => onTransactionReject(tx.safeTxHash)}
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Icon
+                      type="circleCross"
+                      size="md"
+                      color="error"
+                      tooltip="Reject Transaction"
+                    />
+                  </div>
+                )}
               </div>
             </div>
           ))}
