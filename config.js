993616fcbe61f669b3007f1d18fee7f778e019fe
Maadhav Sharma <maadhav2001@gmail.com>
2023-05-19 13:18:01 +0530

    added ticker names

--- config.js
+++ config.js
@@ -5,12 +5,16 @@ export const config = {
       rpcTarget: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
       displayName: "Goerli",
       blockExplorer: "https://goerli.etherscan.io/",
+      ticker: "ETH",
+      tickerName: "ETH",
     },
     avalanche: {
       chainId: "0xA869",
       rpcTarget: "https://api.avax-test.network/ext/bc/C/rpc",
       displayName: "Avalanche Fuji Testnet",
       blockExplorer: "https://testnet.snowtrace.io",
+      ticker: "AVAX",
+      tickerName: "AVAX",
     },
   },
 };
